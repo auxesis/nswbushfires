18a2d403e06de53f72ef47852640b0726456a7b4
Lindsay Holmwood <lindsay@holmwood.id.au>
2011-09-21 08:44:49 +1000

    Filter out newlines in scraped output

--- fetcher.rb
+++ fetcher.rb
@@ -118,7 +118,7 @@ class RFSCurrentIncidentsFetcher < Fetcher
   private
   def find_part(parts, regex)
     part = parts.find {|d| d =~ regex}
-    part ? part.split(/[A-Z]:\s*/).last.strip : nil
+    part ? part.split(/[A-Z]:\s*/).last.strip.gsub("\n", ", ") : nil
   end
 
 end
